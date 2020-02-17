9202d8ec168e1a29eaedf77686dddb04c71493dc
al375729 <57604778+al375729@users.noreply.github.com>
2020-02-17 22:53:41 +0100

    Generacio aleatoria de la estructura del nivel

---
+++ Proyecto Mosqueteros/Assets/LevelGenerator.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+public class LevelGenerator : MonoBehaviour
+{
+
+    private int nivel =0;
+    private int dimension = 11;
+    public GameObject sala;
+    private Quaternion newRotation;
+    private int[,] matriz;
+    //private int numSalas = 10;
+
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+      matriz = new int[dimension + nivel, dimension + nivel];
+        newRotation = Quaternion.Euler(-90, 0, 0);
+
+       
+
+
+        for (int i = 0; i < dimension; i += 1)
+        {
+            for (int j = 0; j < dimension; j += 1)
+            {
+                matriz[i, j] = 0;
+                //print(matriz[i, j]);
+            }
+        }
+
+        //Instantiate(sala, new Vector3(i * 9.0f, 0, 0), newRotation);
+
+        generate();
+    }
+
+
+    void generate()
+    {
+        int ran;
+        int medio1 = (int)Math.Round((dimension / 2) + 0.5);
+        int medio2 = medio1;
+        int control = 0;
+        while (control < 12)
+        {
+            ran = UnityEngine.Random.Range(0, 3);
+            matriz[medio1, medio2] = 1;
+
+            if (ran == 0)
+            {
+                if (medio1 > 0)
+                {
+                    if (matriz[medio1 - 1, medio2] != 1)
+                    {
+                        medio1 -= 1;
+                        matriz[medio1, medio2] = 1;
+                        control += 1;
+                        print(control);
+                    }
+                }
+            }
+
+            else if (ran == 1)
+            {
+                if (medio1 < dimension - 1)
+                {
+                    if (matriz[medio1 + 1, medio2] != 1)
+                    {
+                        medio1 += 1;
+                        matriz[medio1, medio2] = 1;
+                        control += 1;
+                        print(control);
+                    }
+                }
+            }
+
+            else if (ran == 2)
+            {
+                if (medio2 > 0)
+                {
+                    if (matriz[medio1, medio2 - 1] != 1)
+                    {
+                        medio2 -= 1;
+                        matriz[medio1, medio2] = 1;
+                        control += 1;
+                        print(control);
+                    }
+                }
+            }
+
+            if (ran == 3)
+            {
+                if (medio2 < dimension - 1)
+                {
+                    if (matriz[medio1, medio2 + 1] != 1)
+                    {
+                        medio2 += 1;
+                        matriz[medio1, medio2] = 1;
+                        control += 1;
+                        print(control);
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < dimension; i += 1)
+        {
+            for (int j = 0; j < dimension; j += 1)
+            {
+                if (matriz[i, j] == 1)
+                {
+                   Instantiate(sala, new Vector3(i * 9.0f, 0, j * 9.0f), newRotation);
+                   
+                }
+            }
+        }
+    }
+}
+
+
