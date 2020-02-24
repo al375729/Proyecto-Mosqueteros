9655df92b6461893f19afebbeafe615ea1db3975
al375729 <57604778+al375729@users.noreply.github.com>
2020-02-24 02:31:13 +0100

    Pruebas Camara

---
+++ Proyecto Mosqueteros/Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Transform target;
+    public float moveSpeed;
+    public Transform target2;
+
+    // Use this for initialization
+    void Start()
+    {
+        moveSpeed = 10f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        //transform.rotation = target.rotation;
+        
+    }
+}
