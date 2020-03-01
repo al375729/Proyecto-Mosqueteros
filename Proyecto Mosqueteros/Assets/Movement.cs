dba2ad49c00564541ce431fa23bd291dafd49ac9
al375729 <57604778+al375729@users.noreply.github.com>
2020-03-01 18:28:33 +0100

    Disparo y Pruebas Varias

--- Proyecto Mosqueteros/Assets/Movement.cs
+++ Proyecto Mosqueteros/Assets/Movement.cs
@@ -6,18 +6,21 @@ public class Movement : MonoBehaviour
 {
 
     public float moveSpeed;
+  
 
     // Use this for initialization
     void Start()
     {
         moveSpeed = 10f;
+      
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+        transform.Translate(moveSpeed * Input.GetAxis("Horizontal") * Time.fixedDeltaTime, 0f, moveSpeed * Input.GetAxis("Vertical") * Time.fixedDeltaTime);
         //transform.rotation = target.rotation;
         
     }
+
 }
