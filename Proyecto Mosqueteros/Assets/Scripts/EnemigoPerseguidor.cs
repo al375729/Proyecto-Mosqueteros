1657c19747a7a3b7f4b29c36a368b25c811e2630
al375729 <57604778+al375729@users.noreply.github.com>
2020-03-13 16:49:07 +0100

    Todo Listo Para La Entrega
    Correccion de errores y bugs.
    Añadida mirilla.

---
+++ Proyecto Mosqueteros/Assets/Scripts/EnemigoPerseguidor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemigoPerseguidor : MonoBehaviour
+{
+
+    public Transform target;
+    Vector3 lookDirection;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        lookDirection = (target.position - transform.position.normalized);
+        transform.Translate(lookDirection * Time.deltaTime * speed);
+
+    }
+}
